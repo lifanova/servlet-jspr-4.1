2add6af12ddde1e5274ab4ae175eb1e91d287477
vlifanova <vlifanova2@alfabank.ru>
2023-03-25 13:52:52 +0300

    Config is added

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,16 +1,13 @@
 package ru.netology.servlet;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.netology.config.JavaConfig;
 import ru.netology.controller.PostController;
-import ru.netology.repository.PostRepository;
-import ru.netology.service.PostService;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-import static ru.netology.controller.PostController.APPLICATION_JSON;
 
 public class MainServlet extends HttpServlet {
     private static final String API = "/api/posts";
@@ -19,29 +16,10 @@ public class MainServlet extends HttpServlet {
 
     @Override
     public void init() {
-        final var repository = new PostRepository();
-        final var service = new PostService(repository);
-        controller = new PostController(service);
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(JavaConfig.class);
+        controller = applicationContext.getBean(PostController.class);
     }
 
-    //  @Override
-    //  protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-    //resp.getWriter().print("Hello from servlet");
-    //resp.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
-    //resp.setHeader("Content-Type", "application/json");
-//        resp.setContentType(APPLICATION_JSON);
-//
-//        final var path = req.getRequestURI();
-//        if (path.equals(API)) {
-//            controller.all(resp);
-//        } else if (path.matches(API_D)) {
-//            final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
-//            controller.getById(id, resp);
-//        } else {
-//            resp.getWriter().print("Hello from servlet");
-//            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-//        }
-    //   }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
